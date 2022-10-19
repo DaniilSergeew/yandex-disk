1ec995fb7901e564a5b1e658095f0a177361acad
daniilsergeev <danya.sergeev@yandex.ru>
2022-10-19 19:49:27 +0300

    add @NoArgsConstructor annotation

--- src/main/java/com/example/yandexdisk/dto/SystemItemImport.java
+++ src/main/java/com/example/yandexdisk/dto/SystemItemImport.java
@@ -2,11 +2,13 @@ package com.example.yandexdisk.dto;
 
 import com.example.yandexdisk.model.SystemItemType;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Data
+@NoArgsConstructor
 public class SystemItemImport {
     @NotNull
     private String id;
