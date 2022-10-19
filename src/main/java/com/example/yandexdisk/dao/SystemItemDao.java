fc80b47fb9a2fd5283a2005066fbc469c6e8446f
daniilsergeev <danya.sergeev@yandex.ru>
2022-10-19 22:38:51 +0300

    add fields to saving systemItem

--- src/main/java/com/example/yandexdisk/dao/SystemItemDao.java
+++ src/main/java/com/example/yandexdisk/dao/SystemItemDao.java
@@ -24,20 +24,30 @@ public class SystemItemDao implements Dao<SystemItem> {
 
     @Override
     public void save(SystemItem systemItem) {
-        String query = "CREATE (s:SystemItem {type: $0, UUID: $1})";
+        // Todo: подумать о том, как достичь уникальности и можно ли поменять типо primary key в БД
+        // https://neo4j.com/developer/kb/how-to-implement-a-primary-key-property-for-a-label/
+        String query = "CREATE (s:SystemItem {type: $0, UUID: $1, url: $2, date: $3, size: $4})";
         log.info("Trying to connect to the database...");
         try (Connection con = DriverManager
                 .getConnection("jdbc:neo4j:bolt://localhost:7687", "neo4j", "pass");
-             PreparedStatement stmt = con.prepareStatement(query);) {
+             PreparedStatement stmt = con.prepareStatement(query)) {
             log.info("The connection was successful");
+
             stmt.setString(0, systemItem.getSystemItemType().toString());
             stmt.setString(1, systemItem.getId());
+            stmt.setString(2, systemItem.getUrl());
+            stmt.setString(3, systemItem.getDate().toString());
+            stmt.setString(4, systemItem.getSize().toString());
 
             log.info("Trying to execute the query...");
             stmt.executeUpdate();
             log.info("The query is successfully executed");
         } catch (SQLException e) {
             throw new RuntimeException(e);
+        }
+
+        if (systemItem.getParentId() != null) {
+            // Todo: прежде чем писать, хорошо подумай как избежать говны
         }
     }
 
