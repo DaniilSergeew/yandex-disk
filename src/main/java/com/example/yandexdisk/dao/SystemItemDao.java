025b81744f3efcc91512e83d510404ffeacae53a
daniilsergeev <danya.sergeev@yandex.ru>
2022-10-19 16:16:54 +0300

    add SystemItemDao class

---
+++ src/main/java/com/example/yandexdisk/dao/SystemItemDao.java
@@ -0,0 +1,36 @@
+package com.example.yandexdisk.dao;
+
+import com.example.yandexdisk.model.SystemItem;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+public class SystemItemDao implements Dao<SystemItem> {
+    @Override
+    public Optional<SystemItem> getById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<SystemItem> getAllById() {
+        return null;
+    }
+
+    @Override
+    public void save(SystemItem systemItem) {
+
+    }
+
+    @Override
+    public void update(SystemItem systemItem) {
+
+    }
+
+    @Override
+    public void deleteById(SystemItem systemItem) {
+
+    }
+}
