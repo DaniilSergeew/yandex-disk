85a57a3ad44043fafa5d3a15a0f3f36934840301
daniilsergeev <danya.sergeev@yandex.ru>
2022-10-19 16:11:58 +0300

    add SystemItemImportRequest class

---
+++ src/main/java/com/example/yandexdisk/dto/request/SystemItemImportRequest.java
@@ -0,0 +1,17 @@
+package com.example.yandexdisk.dto.request;
+
+import com.example.yandexdisk.dto.SystemItemImport;
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class SystemItemImportRequest {
+    @NotNull
+    private List<SystemItemImport> items;
+
+    @NotNull
+    private LocalDateTime date;
+}
