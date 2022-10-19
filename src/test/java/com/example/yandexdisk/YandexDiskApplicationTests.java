88557ce23befec3e5278421707eaa4241877af4a
daniilsergeev <danya.sergeev@yandex.ru>
2022-10-19 15:55:48 +0300

    initial commit

---
+++ src/test/java/com/example/yandexdisk/YandexDiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yandexdisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YandexDiskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
