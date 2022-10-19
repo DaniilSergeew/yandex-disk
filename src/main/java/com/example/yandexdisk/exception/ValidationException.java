c5e41e5ada1c1d44f9cfda62b88dc486975c2145
daniilsergeev <danya.sergeev@yandex.ru>
2022-10-19 16:10:17 +0300

    add ValidationException class

---
+++ src/main/java/com/example/yandexdisk/exception/ValidationException.java
@@ -0,0 +1,7 @@
+package com.example.yandexdisk.exception;
+
+public class ValidationException extends Exception {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
