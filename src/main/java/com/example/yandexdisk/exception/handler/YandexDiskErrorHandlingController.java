703ffce3f1971ed0fab7a1e3ad078735da6a8d78
daniilsergeev <danya.sergeev@yandex.ru>
2022-10-21 16:13:01 +0300

    add handleEntityNotFoundException

--- src/main/java/com/example/yandexdisk/exception/handler/YandexDiskErrorHandlingController.java
+++ src/main/java/com/example/yandexdisk/exception/handler/YandexDiskErrorHandlingController.java
@@ -1,5 +1,6 @@
 package com.example.yandexdisk.exception.handler;
 
+import com.example.yandexdisk.exception.EntityNotFoundException;
 import com.example.yandexdisk.exception.ValidationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,12 @@ public class YandexDiskErrorHandlingController {
     @ExceptionHandler(ValidationException.class)
     public ResponseEntity<Void> handleValidationException(ValidationException ex) {
         log.error("ValidationException " + ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<Void> handleEntityNotFoundException(EntityNotFoundException ex) {
+        log.error("EntityNotFoundException " + ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
     // Todo: добавить обработчик для sql exception
