9906ce619b6cc61c3e0c65fe3bef2a2aa1f64031
daniilsergeev <danya.sergeev@yandex.ru>
2022-10-21 17:40:18 +0300

    add handlerMethodArgumentNotValidException

--- src/main/java/com/example/yandexdisk/exception/handler/YandexDiskErrorHandlingController.java
+++ src/main/java/com/example/yandexdisk/exception/handler/YandexDiskErrorHandlingController.java
@@ -5,8 +5,10 @@ import com.example.yandexdisk.exception.ValidationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 @Slf4j
@@ -20,6 +22,12 @@ public class YandexDiskErrorHandlingController {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<Void> handleEntityNotFoundException(EntityNotFoundException ex) {
         log.error("EntityNotFoundException " + ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Void> handlerMethodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request) {
+        log.error("MethodArgumentNotValidException " + ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
     // Todo: добавить обработчик для sql exception
