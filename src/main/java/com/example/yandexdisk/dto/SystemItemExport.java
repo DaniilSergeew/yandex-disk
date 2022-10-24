b10873dd3143f1d0b1ea89bf4b24f83b7a90fbab
daniilsergeev <danya.sergeev@yandex.ru>
2022-10-24 16:32:42 +0300

    create SystemItemExport class

---
+++ src/main/java/com/example/yandexdisk/dto/SystemItemExport.java
@@ -0,0 +1,31 @@
+package com.example.yandexdisk.dto;
+
+import com.example.yandexdisk.model.SystemItemType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SystemItemExport {
+    private String id;
+
+    private String url;
+
+    private String parentId;
+
+    private LocalDateTime date;
+
+    private SystemItemType type;
+
+    private Integer size;
+
+    List<SystemItemExport> children;
+
+}
