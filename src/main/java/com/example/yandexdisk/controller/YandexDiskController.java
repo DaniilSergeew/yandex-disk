126f90e18c037ff3e25e044bb47ac305c785b872
daniilsergeev <danya.sergeev@yandex.ru>
2022-10-27 17:00:05 +0300

    improve paths

--- src/main/java/com/example/yandexdisk/controller/YandexDiskController.java
+++ src/main/java/com/example/yandexdisk/controller/YandexDiskController.java
@@ -33,7 +33,7 @@ public class YandexDiskController {
             description = "При удалении папки удаляются все дочерние элементы." +
                     "Доступ к истории обновлений удаленного элемента невозможен."
     )
-    @DeleteMapping("delete/{id}")
+    @DeleteMapping("/delete/{id}")
     public void deleteSystemItems(@PathVariable String id) {
         service.deleteSystemItemsById(id);
     }
@@ -42,8 +42,8 @@ public class YandexDiskController {
             summary = "Получить информацию об элементе по идентификатору.",
             description = "При получении информации о папке также предоставляется информация о её дочерних элементах."
     )
-    @GetMapping("nodes/{id}")
-    public void getNodesById(@PathVariable String id) throws EntityNotFoundException {
+    @GetMapping("/nodes/{id}")
+    public void getNodesById(@PathVariable String id) {
         service.getSystemItemsById(id);
     }
 
