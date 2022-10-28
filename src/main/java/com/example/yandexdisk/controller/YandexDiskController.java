4bbd4a56814c979204d64ad73c5b08cb91450ec2
daniilsergeev <danya.sergeev@yandex.ru>
2022-10-28 15:41:52 +0300

    add ApiResponses and improve type of data

--- src/main/java/com/example/yandexdisk/controller/YandexDiskController.java
+++ src/main/java/com/example/yandexdisk/controller/YandexDiskController.java
@@ -1,12 +1,18 @@
 package com.example.yandexdisk.controller;
 
+import com.example.yandexdisk.dto.SystemItemExport;
 import com.example.yandexdisk.dto.request.SystemItemImportRequest;
 import com.example.yandexdisk.exception.EntityNotFoundException;
 import com.example.yandexdisk.exception.ValidationException;
 import com.example.yandexdisk.service.api.YandexDiskApiService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -23,9 +29,15 @@ public class YandexDiskController {
                     "Изменение типа элемента с папки на файл и с файла на папку не допускается." +
                     "Порядок элементов в запросе является произвольным."
     )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Import sucseful"),
+            @ApiResponse(responseCode = "400", description = "RequestBody incorrect", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Unexpected error", content = @Content)
+    })
     @PostMapping("/imports")
-    public void importSystemItems(@Valid @RequestBody SystemItemImportRequest request) throws ValidationException {
+    public ResponseEntity<?> importSystemItems(@Valid @RequestBody SystemItemImportRequest request) throws ValidationException {
         service.saveSystemItems(request);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @Operation(
@@ -33,18 +45,30 @@ public class YandexDiskController {
             description = "При удалении папки удаляются все дочерние элементы." +
                     "Доступ к истории обновлений удаленного элемента невозможен."
     )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Delete sucseful"),
+            @ApiResponse(responseCode = "400", description = "Id incorrect", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Unexpected error", content = @Content)
+    })
     @DeleteMapping("/delete/{id}")
-    public void deleteSystemItems(@PathVariable String id) {
+    public ResponseEntity<?> deleteSystemItems(@PathVariable String id) {
         service.deleteSystemItemsById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @Operation(
             summary = "Получить информацию об элементе по идентификатору.",
             description = "При получении информации о папке также предоставляется информация о её дочерних элементах."
     )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Getting sucseful"),
+            @ApiResponse(responseCode = "400", description = "Id incorrect", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Unexpected error", content = @Content)
+    })
     @GetMapping("/nodes/{id}")
-    public void getNodesById(@PathVariable String id) {
-        service.getSystemItemsById(id);
+    public ResponseEntity<SystemItemExport> getNodesById(@PathVariable String id) throws EntityNotFoundException {
+        SystemItemExport response = service.getSystemItemsById(id);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 }
