3b4335ad6800773be0aada29bd45b89f8589fd1d
daniilsergeev <danya.sergeev@yandex.ru>
2022-10-28 15:01:14 +0300

    add comment

--- src/main/java/com/example/yandexdisk/exception/EntityNotFoundException.java
+++ src/main/java/com/example/yandexdisk/exception/EntityNotFoundException.java
@@ -1,6 +1,10 @@
 package com.example.yandexdisk.exception;
 
 public class EntityNotFoundException extends Exception {
+    /**
+     *
+     * @param id элемента в БД
+     */
     public EntityNotFoundException(String id) {
         super(String.format("SystemItem %s not found", id));
     }
